df41ae7027d979e4b4929cedfda3bd5fee75a698
kharazi <vahid@kharazi.net>
2019-08-08 15:16:08 +0430

    session 8

---
+++ djangochat/users/serializers.py
@@ -0,0 +1,58 @@
+from users.models import Users
+from rest_framework import serializers
+
+
+class RequestGetSerializer(serializers.Serializer):
+    first_name = serializers.CharField(
+        required=False, allow_blank=False, max_length=100)
+    last_name = serializers.CharField(
+        required=False, allow_blank=False, max_length=100
+    )
+
+    def validate(self, data):
+        if 'first_name' not in data and 'last_name' not in data:
+            raise serializer.ValidationError(
+                'At least one of firstname or lastname parameters are required'
+            ) 
+        return data
+
+
+# class UsersSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Users
+#         fields = ['first_name', 'last_name']
+
+#     def validate_number_of_friends(self, data):
+#         if data == 5:
+#             raise serializers.ValidationError(
+#                 'In this country, you cant have 5 friends')
+#         return data
+
+class UsersSerializer(serializers.Serializer):
+    first_name = serializers.CharField(
+        required=True, allow_blank=False, max_length=100)
+    last_name = serializers.CharField(
+        required=True, allow_blank=False, max_length=100
+    )
+    birthday = serializers.DateField()
+    number_of_friends = serializers.IntegerField(
+        default=10, min_value=0
+    )
+
+    def validate_number_of_friends(self, data):
+        if data == 5:
+            raise serializers.ValidationError(
+                'In this country, you cant have 5 friends')
+        return data
+
+    def create(self, validated_data):
+        print("I'm in the create function")
+        u = Users(
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name'],
+            birthday=validated_data['birthday'],
+            number_of_friends=validated_data['number_of_friends']
+        )
+        u.save()
+        return u
+        
--- djangochat/users/views.py
+++ djangochat/users/views.py
@@ -1,91 +1,46 @@
 from datetime import datetime
+
 from users.models import Users
 from django.http.response import JsonResponse
 
+from users.serializers import UsersSerializer, RequestGetSerializer
 
 
 def user_list_item_view(request):
     if request.method == 'GET':
-        user_list = []
+        request_serializer = RequestGetSerializer(data=request.GET)
+        if request_serializer.is_valid():
 
-        users = Users.objects
+            users = Users.objects
+            if request_serializer.data['first_name']:
+                users = users.filter(
+                    first_name=request_serializer.data['first_name']
+                )
+            if request_serializer.data['last_name']:
+                users = users.filter(
+                    last_name=request_serializer.data['last_name']
+                )
 
-        if 'first_name' in request.GET:
-            users = users.filter(
-                    first_name=request.GET['first_name'])
-
-        if 'last_name' in request.GET:
-            users = users.filter(last_name=request.GET['last_name'])
-
-        if 'first_name' not in request.GET and 'last_name' not in request.GET:
-            users = []
-
-        
-
-        # Users.objects.filter(first_name=request.GET['first_name']).fileter(
-        #     last_name=request.GET['last_name']
-        # )
-
-        # if 'first_name' in request.GET and 'last_name' in request.GET:
-        #     users = Users.objects.filter(
-        #         first_name=request.GET['first_name'],
-        #         last_name=request.GET['last_name']
-        #     )
-        # elif 'first_name' in request.GET and not 'last_name' in request.GET:
-
-        #     users = Users.objects.filter(
-        #         first_name=request.GET['first_name']
-        #     )
-        for u in users:
-            user_list.append(
+            serializer = UsersSerializer(instance=users, many=True)
+            return JsonResponse(
                 {
-                    'first_name': u.first_name,
-                    'last_name': u.last_name,
-                    'id': u.id
+                    'data': serializer.data
                 }
             )
+        else:
+            return JsonResponse(
+                request_serializer.errors,
+                status=400
+            )
 
-        return JsonResponse(
-            {
-                'data': user_list
-            }
-        )
     elif request.method == 'POST':
-        first_name = request.POST['first_name']
-        last_name = request.POST['last_name']
-        number_of_friends = request.POST['number_of_friends']
-        birthday = datetime.fromisoformat(
-            request.POST['birthday']
-        )
-        try:
-            number_of_friends = int(number_of_friends)
-        except ValueError:
-            return JsonResponse({
-                'message': 'number of freinds must be an integer field'
-            }, status=400)
+        serializer = UsersSerializer(data=request.POST)
 
-        if len(first_name) > 100:
-            return JsonResponse({
-                'message': 'first name must be less than 100 chars'
-            }, status=400)
+        if serializer.is_valid():
+            serializer.save()
+        else:
+            return JsonResponse(serializer.errors, status=400)
 
-        u = Users(
-            first_name=first_name,
-            last_name=last_name,
-            number_of_friends=number_of_friends,
-            birthday=birthday
-        )
-        # u.full_clean()
-        u.save()
-
-        return JsonResponse(
-            {
-                'data': {
-                    'first_name': u.first_name,
-                    'last_name': u.last_name,
-                    'birthday': u.birthday,
-                    '#friends': u.number_of_friends,
-                    'id': u.id
-                }
-            }
-        )
+        return JsonResponse({
+            'data': serializer.data
+        })
